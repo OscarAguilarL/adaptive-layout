0d2f459e009bdd049b00856d7ed1a6d6ca631de5
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-11-14 20:17:23 -0600

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:adaptive_layout/src/people.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 600) {
+            return const WideLayout();
+          } else {
+            return const NarrowLayout();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class WideLayout extends StatefulWidget {
+  const WideLayout({super.key});
+
+  @override
+  State<WideLayout> createState() => _WideLayoutState();
+}
+
+class _WideLayoutState extends State<WideLayout> {
+  Person? _person;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: PeopleList(
+              onPersonTap: (person) => setState(() => _person = person)),
+        ),
+        Expanded(
+          flex: 3,
+          child: _person == null ? const Placeholder() : PersonDetail(_person!),
+        ),
+      ],
+    );
+  }
+}
+
+class NarrowLayout extends StatelessWidget {
+  const NarrowLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PeopleList(
+      onPersonTap: (person) => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => Scaffold(
+            appBar: AppBar(),
+            body: PersonDetail(person),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PeopleList extends StatelessWidget {
+  final void Function(Person) onPersonTap;
+
+  const PeopleList({super.key, required this.onPersonTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        for (var person in people)
+          ListTile(
+            leading: Image.network(person.picture),
+            title: Text(person.name),
+            onTap: () => onPersonTap(person),
+          ),
+      ],
+    );
+  }
+}
+
+class PersonDetail extends StatelessWidget {
+  final Person person;
+
+  const PersonDetail(this.person, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(person.name),
+          Text(person.phone),
+        ],
+      ),
+    );
+  }
+}
